f3190970bff5e3d4b2c2bd198401a62f64e2793e
OneAirtel\deepanshunagpal <a_deepanshu.nagpal@airtel.com>
2022-03-29 23:03:59 +0530

    day 1 commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # data-structure
+
+Arrays
+
+1. [Reverse the Array](https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/)
+2. [Find the maximum and minimum element in an array](https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/)
---
+++ src/main/java/Arrays/day_1/KthElement.java
@@ -0,0 +1,19 @@
+package Arrays.day_1;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class KthElement {
+    public static void main(String[] args) {
+        System.out.println("entering");
+        Integer arr [] = {7 ,10 ,  4 , 20 ,15};
+
+        final int i = kthElement(arr, 0, arr.length  , 4 );
+        System.out.println(i);
+    }
+    private static int kthElement(Integer[] arr, int l ,int r , int k){
+        Arrays.sort(arr , Collections.reverseOrder());
+        final int i = r - k;
+        return arr[i];
+    }
+}
